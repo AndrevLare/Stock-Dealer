38ec7dadfc12ff0e66475d540606f8d073aeea7d
Taizoo Avila <riavila@unal.edu.co>
2025-06-13 18:57:51 -0500

    gitignore?

--- main.py
+++ main.py
@@ -9,19 +9,15 @@ class Interface(tk.Tk):
         self.columnconfigure(0, weight=1)
 
         input_ticker = Input(self, "ticker")
-        input_ticker.grid(row = 0, column = 0, sticky="nsew")
-
-        input_exchange = Input(self, "exchange code")
-        input_exchange.grid(row = 1, column = 0, sticky="nsew")
-
+        input_ticker.grid(row = 0, column = 0)
 
 class Input(ttk.Frame):
     def __init__ (self, parent, label): 
         super().__init__()
         self.entry = ttk.Entry(self)
         self.entry.grid(row=0, column=1, padx=30) 
-        self.label = ttk.Label(self, text=f"Enter {label}:")
-        self.label.grid(row=0, column=0)
+        self.label = ttk.Label(self, text=f"Enter {label}:", anchor="w")
+        self.label.grid(row=0, column=0, sticky="w")
 
 
 def main():
